6693f9208c0f974aa865aec3709b777fa466e230
Jimmy Li <jimmyli@m800.com>
2023-08-25 10:28:25 +0800

    feat: add CTA feature example

--- ExampleOC/ExampleOC/ViewController.m
+++ ExampleOC/ExampleOC/ViewController.m
@@ -21,7 +21,62 @@
     // Do any additional setup after loading the view.
     self.widget = [[CinnoxVisitorWidget alloc] initWithFrame:CGRectMake(100, 100, 50, 50)];
     [self.view addSubview: self.widget];
+    
+    UIButton *ctaButton = [[UIButton alloc] init];
+    [ctaButton setTitle:@"CTA" forState:UIControlStateNormal];
+    [ctaButton addTarget:self action:@selector(ctaButtonTap) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:ctaButton];
+
+    ctaButton.translatesAutoresizingMaskIntoConstraints = NO;
+
+    NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:ctaButton
+                                   attribute:NSLayoutAttributeCenterX
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:self.view
+                                   attribute:NSLayoutAttributeCenterX
+                                  multiplier:1.0
+                                    constant:0];
+
+    NSLayoutConstraint *centerY = [NSLayoutConstraint constraintWithItem:ctaButton
+                                   attribute:NSLayoutAttributeCenterY
+                                   relatedBy:NSLayoutRelationEqual
+                                      toItem:self.view
+                                   attribute:NSLayoutAttributeCenterY
+                                  multiplier:1.0
+                                    constant:0];
+
+    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:ctaButton
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1.0
+                                  constant:100];
+
+    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:ctaButton
+                                  attribute:NSLayoutAttributeHeight
+                                  relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                 multiplier:1.0
+                                   constant:50];
+
+    [self.view addConstraints:@[centerX, centerY, width, height]];
 }
 
+- (void)ctaButtonTap {
+    // MARK: create call tag action
+    CinnoxAction *tagAction = [CinnoxAction initTagActionWithTagId:@"YOUR_TAG_ID" contactMethod: CinnoxVisitorContactMethodMessage];
+    
+    // MARK: create call staff action
+    // CinnoxAction *staffAction = [CinnoxAction initStaffActionWithStaffEid:@"YOUR_STAFF_EID" contactMethod: CinnoxVisitorContactMethodMessage];
+    
+    // MARK: create open directory action
+    // CinnoxAction *directoryAction = [CinnoxAction initDirectoryAction];
+    
+    [[CinnoxVisitorCore current] callToActionWithAction:tagAction completion:^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+}
 
 @end
--- ExampleSwift/Example/ViewController.swift
+++ ExampleSwift/Example/ViewController.swift
@@ -16,7 +16,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.addSubview(widget)
+        let ctaButton = UIButton()
+        ctaButton.setTitle("CTA", for: .normal)
+        ctaButton.addTarget(self, action: #selector(ctaButtonTap), for: .touchUpInside)
+        view.addSubview(ctaButton)
+        
+        ctaButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            ctaButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            ctaButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            ctaButton.widthAnchor.constraint(equalToConstant: 100),
+            ctaButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
-
+    
+    @objc
+    func ctaButtonTap() {
+        // MARK: create call tag action
+        let tagAction = CinnoxAction.initTagAction(tagId: "YOUR_TAG_ID", contactMethod: .message)
+        
+        // MARK: create call staff action
+        // let staffAction = CinnoxAction.initStaffAction(staffEid: "YOUR_STAFF_EID", contactMethod: .message)
+        
+        // MARK: create open directory action
+        // let directoryAction = CinnoxAction.initDirectoryAction()
+        CinnoxVisitorCore.current?.callToAction(action: tagAction, completion: { error in
+            print(error)
+        })
+    }
 }
 
